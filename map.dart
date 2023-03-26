b8e6ed9be54d57fa5b31ba54d568db3a159eb6e0
Tim Siwula <{ID}+{username}@users.noreply.github.com>
2023-03-26 16:05:03 -0500

    :bowtie: [auto generated] via tim's deploy_script.sh

--- map.dart
+++ map.dart
@@ -1,9 +1,32 @@
+// run: dart run map.dart
 
-final List<Map<String, Object>> mapping = [
-	{
-	  'page': 'Categories',
-	  'title': 'my title',
-	},
-    ];
+import 'dart:io';
 
+
+void main(List<String> args) async {
+
+	print("running map.dart");
+
+
+	final List<Map<String, Object>> mapping = [
+		{
+			'page': 'Categories',
+			'title': 'my title',
+		},
+	];
+
+	print("mapping = "+ mapping.asMap().toString());
+
+	Map<String, bool> _filters = {
+		'one': false,
+		'two': true,
+		'three': false,
+	};
+
+	print("_filters = "+ _filters.toString());
+
+	List<Object> _items = [];
+
+
+}
 	
